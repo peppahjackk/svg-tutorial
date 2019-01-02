eea3e11b4637de3e8f944f8759eea27f34fa241b
Ted Moke <ted.moke@syneoshealth.com>
2019-01-02 12:27:37 -0500

    Continues exploration

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import anime from 'animejs';
 import Pin from 'pin';
+import Nav from 'nav';
 
 var pins = document.querySelectorAll('.icon.pin');
 
 for (var i = 0; i < pins.length; i++) {
   new Pin(pins[i])
 }
+
+const nav = new Nav();
+
+nav.init();
 
